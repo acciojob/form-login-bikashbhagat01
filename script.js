d88d2db558d8232cda5c627e71aaf8352399c65d
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-17 02:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-
+    // Get form by ID
+    var form = document.getElementById('form1');
+    
+    // Get first and last name values
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+    
+    // Display alert with full name
+    alert(firstName + " " + lastName);
 }
