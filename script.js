ef5bf09a914540e214c78163b458e5bb42eaa7bd
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-17 21:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function getFormValue() {
+function getFormvalue() {
 	const form = document.getElementById("form1");
 
-	const formData = new FormData(form);
-	const firstName = formData.get('fname');
-	const lastName = formData.get('lname');
+	// const formData = new FormData(form);
+	// const firstName = formData.get('fname');
+	// const lastName = formData.get('lname');
+
+	const firstName = form.elements['fname'].value;
+	const lastName = form.elements['lname'].value;
 
 	alert(firstName + " " + lastName)
 }
